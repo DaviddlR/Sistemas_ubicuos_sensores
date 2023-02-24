0e72ea4b35235badd197d3e032027f76e571eefa
David de la Rosa <drr00025@red.ujaen.es>
2023-02-24 17:22:18 +0100

    Estilo de la UI
    Queda por hacer el resto de sensores que no son acelerómetro

--- MainScreen.js
+++ MainScreen.js
@@ -45,7 +45,11 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
+function activarSensor(id){
+  console.log(id)
 
+  
+}
 
 
 export default function App() {
@@ -53,21 +57,76 @@ export default function App() {
   // Datos para la listView (nombres de los sensores)
   const dataSource = [
     { id: 1, title: 'Acelerómetro' },
-    { id: 2, title: 'A' },
+    { id: 2, title: 'Barómetro' },
+    { id: 3, title: 'Giroscopio' },
+    // { id: 4, title: 'Luminosidad' },
+    // { id: 5, title: 'Magnetómetro' },
+    // { id: 6, title: 'Podómetro' },
+    // { id: 7, title: 'GPS' },
     { id: 999}
     
   ];
+  
 
   // Elementos de la listview
   // Función que representa el item de la lista
   const Item = ({ id, title }) => (
 
-    <TouchableOpacity style={styles.item}>
-        <Text style={styles.title}>
-            {title}
-        </Text>
-    </TouchableOpacity>
+    <View style={styles.item}>
+
+      <Text style={styles.title}>
+          {title}
+      </Text>
+
+      <TouchableOpacity style={estilarBoton(id, 0)} onPress={() => {
+        console.log("----", sensores[id])
+        if(sensores[id] == false){
+          
+          setSensores(previousState => {
+            return { ...previousState, [id]:true}
+          })
+          console.log('abrimos')
+          suscripcionsetAcelerometro(
+            Accelerometer.addListener(setData),
+            Accelerometer.setUpdateInterval(400)
+          );
+        } else {
+          setSensores(previousState => {
+            return { ...previousState, [id]:false}
+          })
+          console.log('cerramos')
+          suscripcionacelerometro && suscripcionacelerometro.remove();
+          setSubscription(null);
+        }
+      }
+        
+        
+      }>
+
+
+        <Text style={styles.textoBotonActivar}>{estilarBoton(id, 1)}</Text>
+      </TouchableOpacity>
+
+    </View>
+    
   );
+
+  function estilarBoton(id,tarea){
+    if(sensores[id] == true){
+      if (tarea == 1){
+        return "Desactivar"
+      } else {
+        return styles.botonDesactivarSensor
+      }
+    } else {
+      if (tarea == 1){
+        return "Activar"
+      } else {
+        return styles.botonActivarSensor
+      }
+    }
+  }
+  
 
   // Función para renderizar el item
   const renderItem = ({ item }) => (
@@ -81,21 +140,79 @@ export default function App() {
       )
   }
 
+  
 
 
+  const [sensores, setSensores] = useState(
+    {
+      1:false,
+      2:false,
+      3:false,
+      4:false,
+      5:false,
+      6:false,
+      7:false,
+    }
+  )
 
+  const [suscripciones, setSuscripciones] = useState(
+    {
+      1:null,
+      2:null,
+      3:null,
+      4:null,
+      5:null,
+      6:null,
+      7:null,
+    }
+  )
 
+  const [acelerometro, setAcelerometro] = useState(false)
+  const [barometro, setBarometro] = useState(false)
+  const [giroscopio, setGiroscopio] = useState(false)
+  const [magnetometro, setMagnetometro] = useState(false)
+  const [luminosidad, setLuminosidad] = useState(false)
+  const [podometro, setPodometro] = useState(false)
+  const [gps, setGPS] = useState(false)
 
+  const [suscripcionacelerometro, suscripcionsetAcelerometro] = useState(null)
+  const [suscripcionbarometro, suscripcionsetBarometro] = useState(null)
+  const [suscripciongiroscopio, suscripcionsetGiroscopio] = useState(null)
+  const [suscripcionmagnetometro, suscripcionsetMagnetometro] = useState(null)
+  const [suscripcionluminosidad, suscripcionsetLuminosidad] = useState(null)
+  const [suscripcionpodometro, suscripcionsetPodometro] = useState(null)
+  const [suscripciongps, suscripcionsetGPS] = useState(null)
 
   const [{ x, y, z }, setData] = useState({
     x: 0,
     y: 0,
     z: 0,
   });
+
+
   const [subscription, setSubscription] = useState(null);
 
-  const _slow = () => Accelerometer.setUpdateInterval(1000);
-  const _fast = () => Accelerometer.setUpdateInterval(16);
+  
+
+  const _subscribe = (id) => {
+    setSubscription(
+      Accelerometer.addListener(setData),
+      Accelerometer.setUpdateInterval(400)
+    );
+  };
+
+  const _unsubscribe = (id) => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  // useEffect(() => {
+  //   _subscribe();
+  //   return () => _unsubscribe();
+  // }, []);
+
+
+
 
   const _registrar = async () => {
     console.log("REGISTRAMOS")
@@ -111,21 +228,6 @@ export default function App() {
     }
   }
 
-  const _subscribe = () => {
-    setSubscription(
-      Accelerometer.addListener(setData)
-    );
-  };
-
-  const _unsubscribe = () => {
-    subscription && subscription.remove();
-    setSubscription(null);
-  };
-
-  useEffect(() => {
-    _subscribe();
-    return () => _unsubscribe();
-  }, []);
 
   return (
 
@@ -144,15 +246,6 @@ export default function App() {
       <Text>y: {y}</Text>
       <Text>z: {z}</Text>
       <View>
-        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} >
-          <Text>{subscription ? 'On' : 'Off'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} >
-          <Text>Slow</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={_fast} >
-          <Text>Fast</Text>
-        </TouchableOpacity>
 
         <TouchableOpacity onPress={_registrar} >
           <Text>Registrar</Text>
@@ -170,7 +263,7 @@ const styles = StyleSheet.create({
     //marginTop: StatusBar.currentHeight || 0,
   },
   item: {
-    backgroundColor: 'light-gray',
+    backgroundColor: 'white',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
@@ -183,5 +276,28 @@ const styles = StyleSheet.create({
       //width: 300,
       height: 2,
       backgroundColor: '#d1d1d1'
-  }
+  },
+  botonActivarSensor: {
+    position: 'absolute',
+    backgroundColor: 'green',
+    right: '5%',
+    top: '60%',
+    width: 90,
+    
+  },
+
+  botonDesactivarSensor: {
+    position: 'absolute',
+    backgroundColor: 'red',
+    right: '5%',
+    top: '60%',
+    width: 90,
+    
+  },
+
+  textoBotonActivar: {
+    color: 'white',
+    fontSize: 17,
+    textAlign: 'center'
+  },
 });
