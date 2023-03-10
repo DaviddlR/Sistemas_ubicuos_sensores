97a3ba793bcdb1ed4958e4dc11be677e0cc773b9
David de la Rosa <drr00025@red.ujaen.es>
2023-03-10 15:43:25 +0100

    intento de GPS (mal)

--- App.js
+++ App.js
@@ -1,74 +1,89 @@
-import React, { useState } from 'react';
-import { Component } from "react"
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Image,
-  Button,
- } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet, Button } from 'react-native';
 
-import MainScreen from './MainScreen'
+import * as Location from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+const LOCATION_TRACKING = 'location-tracking';
 
-import 'react-native-gesture-handler';
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
 
-
-const Stack = createStackNavigator();
-
-
- export default class App extends Component {
-
-
-    render() {
-     return (
-
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='MainScreen'
-        
-        screenOptions={{
-          title: "Main",
-          headerTitleAlign: 'center',
-          
-          headerStyle: {
-            backgroundColor: '#003060',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-
-          // NO FUNCIONA -> status bar
-          
-        }}
-        
-        >
-
-            <Stack.Screen
-                name="MainScreen"
-                component={MainScreen}
-                options={{
-                  headerTitle:"Main Screen",
-                }}
-              />
-
-              
-
-        </Stack.Navigator>
-
-      </NavigationContainer>
-
-     );
+  const startLocationTracking = async () => {
+    await Location.startLocationUpdatesAsync(LOCATION_TRACKING, {
+      accuracy: Location.Accuracy.Highest,
+      timeInterval: 5000,
+      distanceInterval: 0,
+    });
+    const hasStarted = await Location.hasStartedLocationUpdatesAsync(
+      LOCATION_TRACKING
+    );
+    console.log('tracking started?', hasStarted);
+  };
 
 
-    }
+
+  useEffect(() => {
+    (async () => {
+
+    console.log("Pide permisos")
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+    console.log("hace cosas")
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
   }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Start tracking" onPress={startLocationTracking} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+TaskManager.defineTask(LOCATION_TRACKING, async ({ data, error }) => {
+    if (error) {
+      console.log('LOCATION_TRACKING task ERROR:', error);
+      return;
+    }
+    if (data) {
+      const { locations } = data;
+      let lat = locations[0].coords.latitude;
+      let long = locations[0].coords.longitude;
+  
+      console.log(
+        `${new Date(Date.now()).toLocaleString()}: ${lat},${long}`
+      );
+    }
+  });
+
+
+
+
+
+
+
